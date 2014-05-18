ce63b786486c2c1af782fbd863c00b2e0612df30
David Jones <dgjones@gmail.com>
2014-05-18 16:43:24 -0700

    put back heroku db type

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ db = URI.parse(ENV['DATABASE_URL'] || 'http://localhost')
 
 if db.scheme == 'postgres' # Heroku environment
   ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme,
+    :adapter  => db.scheme == 'postgres' ? 'em_postgresql' : db.scheme,
     :host     => db.host,
     :username => db.user,
     :password => db.password,
