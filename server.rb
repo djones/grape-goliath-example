ecdd024f34bedd0ae370e3a36afe780520984acc
David Jones <dgjones@gmail.com>
2014-05-18 17:24:02 -0700

    simplified require call

--- server.rb
+++ server.rb
@@ -3,10 +3,10 @@ require 'em-synchrony/activerecord'
 require 'grape'
 require 'erb'
 
-# replace with an load anything in app statement
-require './app/apis/posts'
-require './app/models/post'
+# Load in any app files you might add
+Dir['./app/**/*.rb'].each { |file| require file }
 
+# Connect to the database
 # ERB needed because of Heroku: http://stackoverflow.com/a/18139387
 db_config = YAML.load(ERB.new(File.read(File.join('config', 'database.yml'))).result)
 ActiveRecord::Base.establish_connection(db_config['production'])
@@ -14,7 +14,7 @@ ActiveRecord::Base.establish_connection(db_config['production'])
 class Application < Goliath::API
 
   def response(env)
-    ::Posts.call(env)
+    Posts.call(env)
   end
 
 end
